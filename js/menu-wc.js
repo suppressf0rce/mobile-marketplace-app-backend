dbd251af36f88684b8041be804d2263725471b82
suppressf0rce <suppressf0rce@users.noreply.github.com>
2023-04-11 00:36:18 +0000

    deploy: e2c95e70d488600c5a83ff1e99741a4697243b31

--- js/menu-wc.js
+++ js/menu-wc.js
@@ -69,8 +69,69 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                             <li class="link">
                                 <a href="modules/CoreModule.html" data-type="entity-link" >CoreModule</a>
                             </li>
+                            <li class="link">
+                                <a href="modules/UserModule.html" data-type="entity-link" >UserModule</a>
+                                <li class="chapter inner">
+                                    <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ?
+                                        'data-target="#injectables-links-module-UserModule-a330fc65ad52c116ce2ea4ff77aaa9caaaed01c14d0fe96f1ec7a078f7ddc1386034c89f9a74eec45bb33d4fa93daf48f9beb9cd804ac5238cd7e6b9c625f277"' : 'data-target="#xs-injectables-links-module-UserModule-a330fc65ad52c116ce2ea4ff77aaa9caaaed01c14d0fe96f1ec7a078f7ddc1386034c89f9a74eec45bb33d4fa93daf48f9beb9cd804ac5238cd7e6b9c625f277"' }>
+                                        <span class="icon ion-md-arrow-round-down"></span>
+                                        <span>Injectables</span>
+                                        <span class="icon ion-ios-arrow-down"></span>
+                                    </div>
+                                    <ul class="links collapse" ${ isNormalMode ? 'id="injectables-links-module-UserModule-a330fc65ad52c116ce2ea4ff77aaa9caaaed01c14d0fe96f1ec7a078f7ddc1386034c89f9a74eec45bb33d4fa93daf48f9beb9cd804ac5238cd7e6b9c625f277"' :
+                                        'id="xs-injectables-links-module-UserModule-a330fc65ad52c116ce2ea4ff77aaa9caaaed01c14d0fe96f1ec7a078f7ddc1386034c89f9a74eec45bb33d4fa93daf48f9beb9cd804ac5238cd7e6b9c625f277"' }>
+                                        <li class="link">
+                                            <a href="injectables/PermissionService.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules" >PermissionService</a>
+                                        </li>
+                                        <li class="link">
+                                            <a href="injectables/RoleService.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules" >RoleService</a>
+                                        </li>
+                                        <li class="link">
+                                            <a href="injectables/UserService.html" data-type="entity-link" data-context="sub-entity" data-context-id="modules" >UserService</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </li>
                 </ul>
                 </li>
+                        <li class="chapter">
+                            <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ? 'data-target="#entities-links"' :
+                                'data-target="#xs-entities-links"' }>
+                                <span class="icon ion-ios-apps"></span>
+                                <span>Entities</span>
+                                <span class="icon ion-ios-arrow-down"></span>
+                            </div>
+                            <ul class="links collapse " ${ isNormalMode ? 'id="entities-links"' : 'id="xs-entities-links"' }>
+                                <li class="link">
+                                    <a href="entities/Permission.html" data-type="entity-link" >Permission</a>
+                                </li>
+                                <li class="link">
+                                    <a href="entities/Role.html" data-type="entity-link" >Role</a>
+                                </li>
+                                <li class="link">
+                                    <a href="entities/User.html" data-type="entity-link" >User</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="chapter">
+                            <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ? 'data-target="#injectables-links"' :
+                                'data-target="#xs-injectables-links"' }>
+                                <span class="icon ion-md-arrow-round-down"></span>
+                                <span>Injectables</span>
+                                <span class="icon ion-ios-arrow-down"></span>
+                            </div>
+                            <ul class="links collapse " ${ isNormalMode ? 'id="injectables-links"' : 'id="xs-injectables-links"' }>
+                                <li class="link">
+                                    <a href="injectables/PermissionService.html" data-type="entity-link" >PermissionService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/RoleService.html" data-type="entity-link" >RoleService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/UserService.html" data-type="entity-link" >UserService</a>
+                                </li>
+                            </ul>
+                        </li>
                     <li class="chapter">
                         <div class="simple menu-toggler" data-toggle="collapse" ${ isNormalMode ? 'data-target="#miscellaneous-links"'
                             : 'data-target="#xs-miscellaneous-links"' }>
